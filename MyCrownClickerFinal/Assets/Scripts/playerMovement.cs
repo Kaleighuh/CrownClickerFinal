c98fd0209ff267345b109451f0bf0c61a0d85cfe
Kaleah <10958501@uvu.edu>
2022-11-08 20:55:34 -0700

    Character Animation Movement

---
+++ MyCrownClickerFinal/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");  //input
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);  //movement
+    }
+}
