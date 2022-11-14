9656ff6c8c366881f4e6c68720dba6339020b17f
Kaleah <10958501@uvu.edu>
2022-11-13 19:38:13 -0700

    Working on Dialogue... many issues

--- MyCrownClickerFinal/Assets/Scripts/Dialogue/DialogManager.cs
+++ MyCrownClickerFinal/Assets/Scripts/Dialogue/DialogManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
+	public Text nameText;
+	public Text dialogueText;
+
 	private Queue<string> sentences;
 
     void Start()
@@ -12,7 +16,30 @@ public class DialogManager : MonoBehaviour
     }
 	public void StartDialogue (Dialogue dialogue)
 	{
-		Debug.Log("Starting conversation");
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+		
+		foreach (string sentence in dialogue.sentences)
+			{
+				sentences.Enqueue(sentence);
+			}
+		DisplayNextSentence();
+	}
+	
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+		string sentence = sentences.Dequeue();
+		dialogueText.text = sentence;
+	}
+	void EndDialogue()
+	{
+		Debug.Log("End of the Conversation.");
 	}
 
 }
