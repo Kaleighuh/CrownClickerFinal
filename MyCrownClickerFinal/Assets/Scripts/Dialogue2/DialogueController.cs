49babacf794f8714cd1ee1f66418cbab15a03a41
Kaleah <10958501@uvu.edu>
2022-12-02 03:26:40 -0700

    Dialogue... again

--- MyCrownClickerFinal/Assets/Scripts/Dialogue2/DialogueController.cs
+++ MyCrownClickerFinal/Assets/Scripts/Dialogue2/DialogueController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DialogueController : MonoBehaviour
+public class DialogueController : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public DialogueUI DialogueUI; 
+    public DialogueSOs CurrentDialogueObject;
+
+    private void OnEnable()
     {
-        
+        DialogueUI.NextButton.onClick.AddListener(OnInteract);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        DialogueUI.NextButton.onClick.RemoveListener(OnInteract);
+    }
+
+    public void OnInteract()
+    {
+        Debug.Log("Interact");
+        DialogueUI.ShowText(CurrentDialogueObject.GetNextDialogue(), true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       // Oninteract();
     }
 }
---
+++ MyCrownClickerFinal/Assets/Scripts/Dialogue2/DialogueUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class DialogueUI : MonoBehaviour
+{
+    public event System.Action TypingTextEnded;
+    public Text DialogueBoxText;
+    public Button NextButton;
+    public float TypeTextDelay = 0.05f;
+
+    public void ShowText(string text, bool shouldType)
+    {
+        this.gameObject.SetActive(true);
+        if (shouldType)
+        {
+            StartCoroutine(TypeText(text));
+        }
+        else
+        {
+            DialogueBoxText.text = text;
+        }
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        string fullText = text;
+        string currentText;
+        for (int i = 0; i < fullText.Length + 1; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            DialogueBoxText.text = currentText;
+            yield return new WaitForSeconds(TypeTextDelay);
+        }
+
+        TypingTextEnded.Invoke();
+        yield return null;
+    }
+}
