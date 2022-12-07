0ae69e7d88c06f55d6aac2c1518af1280771e2d3
Kaleah <10958501@uvu.edu>
2022-12-06 17:29:07 -0700

    Singleton and more manual touch inputs

--- MyCrownClickerFinal/Assets/Inputs/InputsManager.cs
+++ MyCrownClickerFinal/Assets/Inputs/InputsManager.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 
-public class InputsManager : MonoBehaviour
+[DefaultExecutionOrder(-1)]
+public class InputsManager : Singleton<InputsManager>
 {
+    public delegate void StartTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnStartTouch;
+    public delegate void EndTouchEvent(Vector2 position, float time);
+    public event EndTouchEvent OnEndTouch;
     private Inputs touchControls;
 
     private void Awake()
@@ -15,27 +21,41 @@ public class InputsManager : MonoBehaviour
     private void OnEnable()
     {
         touchControls.Enable();
+        TouchSimulation.Enable();
+       // UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += FingerDown;
+
     }
 
     private void OnDisable()
     {
         touchControls.Disable();
+        TouchSimulation.Disable();
+        //UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= FingerDown;
+
     }
 
     private void Start()
     {
         touchControls.Touch.TouchPress.started += ctx => TouchStart(ctx);
         touchControls.Touch.TouchPress.canceled += ctx => TouchEnd(ctx);
-
     }
 
     private void TouchStart(InputAction.CallbackContext context)
     {
         Debug.Log("Touch started" + touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+        if (OnStartTouch != null)
+            OnStartTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.startTime);
     }
 
     private void TouchEnd(InputAction.CallbackContext context)
     {
         Debug.Log("Touch Ended");
+        if (OnEndTouch != null)
+            OnEndTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.time);
+    }
+    
+    private void FingerDown()
+    {
+        
     }
 }
